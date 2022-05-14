c364a2c30fcc4e7e3ea109461a861fe8fc69ec7d
Ahmed Saeed <ahmedsaeedramadan@yahoo.com>
2022-05-14 17:37:11 +0200

    Update zipuploadtele.py

--- zipuploadtele.py
+++ zipuploadtele.py
@@ -1,7 +1,7 @@
 import shutil, sys, requests
 
 def helpmsg():
-        print('{} filedir zipfile'.format(sys.argv[0]))
+        print('usage: {} filedir zipfile'.format(sys.argv[0]))
 
 def sendtele(webhookurl, url):
         messaget = webhookurl + 'the link is :' + url
@@ -23,6 +23,9 @@ def uploadanon(file):
         return data['data']['file']['url']['short']
 
 if __name__ == '__main__':
+        if len(sys.argv) < 2:
+                helpmsg()
+                exit()
         shutil.make_archive(sys.argv[2], format='zip', root_dir>
         anonlnk = uploadanon(sys.argv[2])
         sendtele('<webhook-telegram>', anonlnk)
